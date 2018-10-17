eb4a893df1a50a2bce263df9cc410978ec460dd2
Miguel Alcantara <miguel.alc@gmail.com>
2018-10-16 23:37:00 -0500

    Enhancing POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/caos/main/user/UserResource.java
+++ src/main/java/com/caos/main/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.caos.main.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,8 +29,16 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public User addUser(@RequestBody User user) {
-		return userDaoService.save(user);
+	public ResponseEntity<User> addUser(@RequestBody User user) {
+		
+		User savedUser = userDaoService.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
