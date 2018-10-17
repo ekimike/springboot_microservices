44b0309300da41294840b95989c23f3cfc94fab3
Miguel Alcantara <miguel.alc@gmail.com>
2018-10-16 19:29:48 -0500

    Clean project

---
+++ src/main/java/com/caos/main/SpringbootMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.caos.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroservicesApplication.class, args);
+	}
+}
