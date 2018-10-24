946df81ad75ad2371c6b7282e232a0de499c845a
Miguel Alcantara <miguel.alc@gmail.com>
2018-10-23 23:15:35 -0500

    JPA Crud

---
+++ src/main/java/com/caos/main/user/UserJPAResource.java
@@ -0,0 +1,106 @@
+package com.caos.main.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserJPAResource {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	@GetMapping("/jpa/users")
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/jpa/users/{id}")
+	public Resource<User> getUser(@PathVariable int id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		
+		Resource<User> resource = new Resource<User>(user.get());
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
+	}
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		
+		userRepository.deleteById(id);
+		
+	}
+	
+	@PostMapping("/jpa/users")
+	public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+		
+		User savedUser = userRepository.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+	
+	@GetMapping("/jpa/users/{id}/posts")
+	public List<Post> getAllUsers(@PathVariable Integer id) {
+		Optional<User> userOptional = userRepository.findById(id);
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("id-"+id);
+		}
+	
+		return userOptional.get().getPosts();
+	}
+	
+	@PostMapping("/jpa/users/{id}/posts")
+	public ResponseEntity<User> createPost(
+			@PathVariable Integer id
+			,@RequestBody Post post) {
+		
+		Optional<User> userOptional = userRepository.findById(id);
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		
+		User user = userOptional.get();
+		post.setUser(user);
+		
+		postRepository.save(post);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(post.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+	
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ insert into user values(102, sysdate(), 'Jorge');
 insert into user values(103, sysdate(), 'Ronald');
 insert into user values(104, sysdate(), 'Yulia');
 insert into user values(105, sysdate(), 'Oscar');			
+
+insert into post values(1001, 'my first post', 101);
+insert into post values(1002, 'my second post', 101);
+insert into post values(1003, 'my first post', 102);
